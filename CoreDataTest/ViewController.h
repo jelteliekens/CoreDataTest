9d1bfcd3fcdbfd91b0f46af9bac742e17bbd64d3
Jelte Liekens <jelteliekens@pandora.be>
2013-11-19 19:26:20 +0100

    Initial Commit

---
+++ CoreDataTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CoreDataTest
+//
+//  Created by Jelte Liekens on 19/11/13.
+//  Copyright (c) 2013 Jelte Liekens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CoreDataTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreDataTest
+//
+//  Created by Jelte Liekens on 19/11/13.
+//  Copyright (c) 2013 Jelte Liekens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
